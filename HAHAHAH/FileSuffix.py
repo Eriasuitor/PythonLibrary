cea5700e3f3f2dae86167d7b3a4de5ccf3eb18ce
lj60 <lory.y.jiang@newegg.com>
2018-01-10 15:30:22 +0800

    Add FileSuffix.py

---
+++ HAHAHAH/FileSuffix.py
@@ -0,0 +1,13 @@
+import os, re
+from urllib import request
+
+path = 'D:\\img'
+url = 'http://10.1.24.133/EaaS/Message/'
+files = os.listdir(path)
+urls = []
+for file in files:
+    # with open(os.path.join(path, file), 'rb') as f:
+    #     req = request.Request(url + file, f.read())
+    #     res = request.urlopen(req).read().decode('utf-8')
+    urls.append(url + file)
+print(urls)
