afdc5ce5fa606285c53f20d2d1841da89f27454b
lj60 <lory.y.jiang@newegg.com>
2018-01-12 10:07:14 +0800

    FileSuffix.py changed.

--- HAHAHAH/FileSuffix.py
+++ HAHAHAH/FileSuffix.py
@@ -1,11 +1,12 @@
 import os, re
 from urllib import request
 
-path = 'D:\\img'
+path = 'D:\\img2 - Copy'
 url = 'http://10.1.24.133/EaaS/Message/'
 files = os.listdir(path)
 urls = []
 for file in files:
+    # print('Start to upload ' + file)
     # with open(os.path.join(path, file), 'rb') as f:
     #     req = request.Request(url + file, f.read())
     #     res = request.urlopen(req).read().decode('utf-8')
