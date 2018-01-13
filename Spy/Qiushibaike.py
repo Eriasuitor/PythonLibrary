5fbbba7415f29cc74730dbc6890b0f8c5fc84967
lj60 <lory.y.jiang@newegg.com>
2018-01-13 17:47:20 +0800

    Add Qiushibaike.py

--- Spy/Qiushibaike.py
+++ Spy/Qiushibaike.py
@@ -7,8 +7,21 @@ proxyHandler = urllib2.ProxyHandler({'http':'http://194.126.37.93:8080'})
 opener = urllib2.build_opener(proxyHandler)
 resp = opener.open(req).read()
 # print repr(resp)
-divs = re.findall(r'<div class="article block.*?>((.{0:5}<div>.*?</div>.{0:5})*)</div>', resp, re.S)
-i = 0
-print divs.__len__()
-for div in divs:
-    print div
+divs = re.finditer(r'article\sblock.*?author\sclearfix.*?'
+                 r'<a.*?'
+                 r'<h2>\s?(?P<author>.*?)\s?</h2>.*?'
+                 r'content".*?<span>\s*(?P<content>.*?)\s*</span>\s*</div>.*?</div>'
+                 , resp, re.S|re.X)
+count = 1
+# for it in divs:
+#     print (str(count) + it.group('author') + ':\n').decode('utf-8').encode('gbk')
+#     count += 1
+#     print it.group('content').replace('<br/>', '\n').decode('utf-8').encode('gbk')
+#     print
+#
+for it in divs:
+    if(it.group().find('class="thumb"') == -1):
+        print (str(count) + it.group('author') + ':\n')
+        count += 1
+        print it.group('content').replace('<br/>', '\n')
+        print
