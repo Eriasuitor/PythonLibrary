dad9c1158cc9981d9c99f7132acaf02f703b88f9
Eriasuitor <funtree9@icloud.com>
2018-01-17 20:56:26 +0800

    Modify class.py

--- Learning/M_Class.py
+++ Learning/M_Class.py
@@ -33,6 +33,7 @@ class Father2(Ancestors):
 class Father(Ancestors):
     age = 23
     salary = 2700.00
+
     def __init__(self):
         print('By father')
         print(self)
@@ -45,14 +46,51 @@ class Father(Ancestors):
     def f2(self):
         print("father's f2:", self)
 
+    def p1(self):
+        print('p property method of Father')
+        return 1
+
+    def p2(self, value):
+        print('p property_setter method of Father')
+
+    def p3(self):
+        print('p property_deleter method of Father')
+
+    p = property(fget=p1, fset=p2, fdel=p3)
+
 
 class Son(Father2, Father):
+    bonus = 20000
+
     def __init__(self, *arg, **args):
         print('By song')
         print(self)
         print(arg)
         print(args)
+        print(self.salary)
         self.salary = 7000.00
+
+    @staticmethod       # Static method
+    def static_method():
+        print('Static method of Son')
+
+    @classmethod
+    def class_method(cls):
+        print('class method of Son')
+        print(cls)
+
+    @property
+    def property_method(self):
+        print('property method of Son')
+        return 1
+
+    @property_method.setter
+    def property_method(self, value):       # Have to add value
+        print('property_setter method of Son')
+
+    @property_method.deleter
+    def property_method(self):
+        print('property_deleter method of Son')
 
     def f2(self):
         print("son's f2:", self)
@@ -64,18 +102,69 @@ class Son(Father2, Father):
         print(self.salary)
 
 
-c1 = Son(1, 2, 'a', eria='Lory')
-c1.f1()
-c1.f2()
+# c1 = Son(1, 2, 'a', eria='Lory')
+# c1.f1()
+# c1.f2()
+# print(Father.salary)        # Not changed, it's static argument
+# print(c1.bonus)
+# print(id(c1.bonus))
+# c1.bonus = 12
+# print(c1.bonus)
+# print(id(c1.bonus))     # id changed
+# print(Son.bonus)
+# print(id(Son.bonus))
+# Son.static_method()
+# Son.class_method()      # <class '__main__.Son'>
+# print(c1.property_method)       # Call like a parameter
+# c1.property_method = 555
+# del c1.property_method
+# c1.p
+# c1.p = 123
+# del c1.p
 '''
     By song
-    <__main__.Son object at 0x000001C1D3A82668>
+    <__main__.Son object at 0x0000027358432A58>
     (1, 2, 'a')
     {'eria': 'Lory'}
-    father's f1: <__main__.Son object at 0x000001C1D3A82668>
-    son's f2: <__main__.Son object at 0x000001C1D3A82668>
-    father's f2: <__main__.Son object at 0x000001C1D3A82668>
-    father's f2: <__main__.Son object at 0x000001C1D3A82668>
+    father2's f1: <__main__.Son object at 0x0000027358432A58>
+    Start to call f2 in Father2
+    son's f2: <__main__.Son object at 0x0000027358432A58>
+    father2's f2: <__main__.Son object at 0x0000027358432A58>
+    father's f2: <__main__.Son object at 0x0000027358432A58>
     male
     23
+    7000.0
+    son's f2: <__main__.Son object at 0x0000027358432A58>
+    father2's f2: <__main__.Son object at 0x0000027358432A58>
+    father's f2: <__main__.Son object at 0x0000027358432A58>
+    male
+    23
+    7000.0
+
 '''
+
+
+class Permissions:
+    __i = 152       # Private
+
+    def __f1(self):     # Private
+        print("Yes")
+
+    def __init__(self):
+        print('init')
+
+    def __call__(self, *args, **kwargs):
+        print('call')
+
+    def __int__(self):
+        print('int')
+        return 1
+
+    def __str__(self):
+        print('str')
+        return 'i love you'
+
+p = Permissions()
+p()
+print(int(p))
+print(str(p))
--- Main.py
+++ Main.py
@@ -8,5 +8,10 @@
 #     print("Wrong, the right number is 8")
 # print('Game Over!')
 
-s = 'i=%E5%AD%98%E5%82%A8%E5%BA%93&from=AUTO&to=AUTO&smartresult=dict&client=fanyideskweb&salt=1514989455443&sign=4b926541b45fa94ffeb04acd2aa51a1c&doctype=json&version=2.1&keyfrom=fanyi.web&action=FY_BY_CLICKBUTTION&typoResult=false'
-print(s.__len__())
+# s = 'i=%E5%AD%98%E5%82%A8%E5%BA%93&from=AUTO&to=AUTO&smartresult=dict&client=fanyideskweb&salt=1514989455443&sign=4b926541b45fa94ffeb04acd2aa51a1c&doctype=json&version=2.1&keyfrom=fanyi.web&action=FY_BY_CLICKBUTTION&typoResult=false'
+# print(s.__len__())
+
+a = 1
+print(type(a))
+a = '123'
+print(type(a))
