3a2e417ad3c213b446627b4391e6fa8ccdcc710f
Eriasuitor <funtree9@icloud.com>
2018-01-23 21:39:45 +0800

    Modified M_Class.py

--- Learning/M_Class.py
+++ Learning/M_Class.py
@@ -23,6 +23,8 @@ class Father2(Ancestors):
     def f1(self):
         print("father2's f1:", self)
         self.sex = 'male'
+        super(Father2, self).f1()
+        Ancestors.f1(self)
         print('Start to call f2 in Father2')
         self.f2()
 
@@ -151,6 +153,7 @@ class Permissions:
         print("Yes")
 
     def __init__(self):
+        self.name = 'Lory'
         print('init')
 
     def __call__(self, *args, **kwargs):
@@ -164,7 +167,88 @@ class Permissions:
         print('str')
         return 'i love you'
 
-p = Permissions()
-p()
-print(int(p))
-print(str(p))
+    # __add__(self, other)
+    # __del__(self) Destruct method
+    # __dict__()
+    def __getitem__(self, item):    # p[item]: # Can return
+        print(type(item))
+        print(item)
+        print(item.start)   # click slice to see more.
+    # __setitem__(self, key, value) # Cannot    p[key] = value
+    # __delitem__(self, key)    # Cannot, Del p[key]
+    # __iter__(self)        # Iterate the result of this method, list, string is same as this. return a iterator
+    # such as iter([1, 2, 3]) or yield. Own this method means iterable object.
+
+    def __iter__(self):
+        for i in range(10):
+            print(i, end=' ')
+            yield i
+
+
+# p = Permissions()       # p is an object of Permissions, Permissions is an object of type
+# # Every class inherit object class
+# p()
+# print(int(p))
+# print(str(p))
+# print(p.__dict__)
+# print(Permissions.__dict__)     # <class 'slice'>
+# p[1: 2: 3]         # slice(1, 2, 3)
+# for i in p:
+#     pass        #　0 1 2 3 4 5 6 7 8 9
+
+# string = 'Lory is so handsome.'
+# print(string[1: 20: 2])     # oyi ohnsm.
+
+# meta class
+
+
+# class Foo:
+#
+#     def f1(self):
+#         print(1)
+
+# -----Like----Below--------------
+
+
+# def f2(self):
+#     print('123Hello')
+#
+#
+# Foo = type('Foo', (object,), {'f1': f2})    # Class name Foo, inherit object, f1 method.
+#
+# foo = Foo()
+# foo.f1()        # 123Hello, all class is an object of type.
+
+
+class MyType(type):
+
+    def __init__(cls, *args, **kwargs):
+        print("MyType's inited method")
+        super(MyType, cls).__init__(cls)
+
+    def __call__(cls, *args, **kwargs):
+        print("MyType's call method")
+        obj = cls.__new__(cls, *args, **kwargs)
+        obj.__init__(cls, *args, **kwargs)
+        return obj      # Currently i believe that this is most reasonable explain.
+
+
+class Foo2(object, metaclass=MyType):       # Declare that generate an object using MyType, not type anymore.
+
+    # __metaclass__ = MyType    # Python2 spelled this.
+
+    def __new__(cls, *args, **kwargs):
+        print("Foo2's new method")
+        return object.__new__(cls, *args, **kwargs)       # This object is foo2!
+
+    def __init__(self, *args, **kwargs):
+        print("Foo2's init method")
+
+    def f1(self):
+        print("Foo2's f1 method")
+
+# output MyType's inited method. because you invoke MyType('Foo2', (object,), ...)
+
+
+foo2 = Foo2()       # MyType's inited method    MyType's call method    Foo2's new method   Foo2's init method
+foo2.f1()
