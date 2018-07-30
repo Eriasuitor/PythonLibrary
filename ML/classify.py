24c9a72de4e443bdccf8611a9a22498302d99634
Eriasuitor <funtree9@icloud.com>
2018-07-30 20:10:37 +0800

    ai

---
+++ ML/classify.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+    layer_name = 'layer%s' % n_layer
+    with tf.name_scope('layer'):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+            tf.summary.histogram(layer_name + '/weights', Weights)
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+            tf.summary.histogram(layer_name + '/biases', biases)
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)
+        tf.summary.histogram(layer_name + '/outputs', outputs)
+        return outputs
+
+def compute_accuracy(v_xs, v_ys):
+    global prediction
+    y_pre = sess.run(prediction, feed_dict={xs: v_xs})
+    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
+    return result
+
+xs = tf.placeholder(tf.float32, [None, 784])  # 28x28
+ys = tf.placeholder(tf.float32, [None, 10])
+
+prediction = add_layer(xs, 784, 10, activation_function=tf.nn.softmax)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
+                                              reduction_indices=[1]))  # loss
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.Session()
+
+# merged= tf.merge_all_summaries()    # tensorflow < 0.12
+merged = tf.summary.merge_all()  # tensorflow >= 0.12
+
+# writer = tf.train.SummaryWriter('logs/', sess.graph)    # tensorflow < 0.12
+writer = tf.summary.FileWriter("logs/", sess.graph)  # tensorflow >=0.12
+
+# sess.run(tf.initialize_all_variables()) # tf.initialize_all_variables() # tf 马上就要废弃这种写法
+sess.run(tf.global_variables_initializer())  # 替换成这样就好
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+    if i % 50 == 0:
+        with tf.name_scope('train'):
+            ca = compute_accuracy(mnist.test.images, mnist.test.labels)
+            tf.summary.scalar('loss', ca)
